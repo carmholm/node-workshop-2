ba2c00c7321e2aa46b330e8b72847c4fed005790
carmholm <ameliacarmenholmes@gmail.com>
2016-04-19 19:16:02 +0000

    Callback-Receiving Functions (higher order functions)

---
+++ call-callbacks.js
@@ -0,0 +1,29 @@
+//Create a function called firstChar that takes a string and a callback, and “returns” the first character of the string after one second.
+// Create a function called lastChar that takes a string and “returns” the last character of the string after one second.
+//Create a function called getFirstAndLast that takes a string and “returns” the first+last character of the string.
+
+function firstChar(str, callback) {
+    var strFirstChar = str.charAt(0);
+    setTimeout(function() {
+        callback(strFirstChar);
+    }, 1000);
+}
+
+function lastChar(str, callback) {
+    var strLastChar = str.charAt(str.length - 1);
+    setTimeout(function() {
+        callback(strLastChar);
+    }, 1000);
+}
+
+function getFirstAndLast(str, callback) {
+    firstChar(str, function(str1){
+        lastChar(str, function(str2){
+            callback(str1 + str2);
+        });
+    });
+}
+
+getFirstAndLast("hello", function(firstLast) {
+    console.log(firstLast);
+  });
